0ae3b2d6f8cdf7e25b0a5e49cb0d444f32c10d20
Andrew Shi <shiandrewlw@gmail.com>
2026-03-01 16:01:22 -0800

    Refactor project identifiers and restore database configuration credentials

--- src/com/fablix/util/DbConfig.java
+++ src/com/fablix/util/DbConfig.java
@@ -1,10 +1,10 @@
 package com.fablix.util;
 
 public final class DbConfig {
-//    public static final String USER = "root";
-//    public static final String PASSWORD = "Tghdfj123!";
-    public static final String USER = "mytestuser";
-    public static final String PASSWORD = "My6$Password";
+    public static final String USER = "root";
+    public static final String PASSWORD = "Tghdfj123!";
+//    public static final String USER = "mytestuser";
+//    public static final String PASSWORD = "My6$Password";
     public static final String URL =
             "jdbc:mysql://localhost:3306/moviedb" +
                     "?useSSL=false" +
